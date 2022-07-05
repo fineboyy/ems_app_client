f229c5449dea8c6a382bef56d8acb87d34f63a13
fineboyy <maqweku@gmail.com>
2022-07-05 15:46:26 +0000

    Added Heroku API Links

--- src/api/employees.js
+++ src/api/employees.js
@@ -1,6 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
-const url = "http://localhost:5000/employees"
+
+let url = ""
+if(process.env.NODE_ENV === "development") {
+    console.log("DEVELOPMENT")
+    url = "http://localhost:5000/employees"
+} else {  
+    console.log("PRODUCTION")
+    url = "https://mighty-peak-10043.herokuapp.com/employees"
+}
 
 export const getAllEmployees = () => axios.get(url)
 export const createNewEmployee = (newEmployee) => axios.post(url, newEmployee)
